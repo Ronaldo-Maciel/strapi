40e9aed71534910d341770b24ccdb4c826931e90
rsmaciel <rsmaciel@recordtv.com.br>
2021-02-23 23:01:13 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a73e300cc321a2e1d11020f1c7dee010'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13cc25ab-d6fd-4c23-9f01-9bc7098b25e4'
+};
